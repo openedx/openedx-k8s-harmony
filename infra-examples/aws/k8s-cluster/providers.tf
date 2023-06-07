2782f08840dea90ebd3534f2d842ab6a926dd717
lpm0073 <lpm0073@gmail.com>
2023-06-07 08:17:39 -0600

    feat: add an aws provider so that local kubeconfig is not explicitly required

--- infra-examples/aws/k8s-cluster/providers.tf
+++ infra-examples/aws/k8s-cluster/providers.tf
@@ -15,6 +15,11 @@
 # Required by Karpenter
 data "aws_partition" "current" {}
 
+# Configure the AWS Provider
+provider "aws" {
+  region = var.aws_region
+}
+
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
