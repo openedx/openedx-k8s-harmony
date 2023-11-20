a3a86066514ea6885373190af2f234155ab8748a
Emad Rad <codewithemad@gmail.com>
2023-11-20 23:19:45 +0330

    fix: pin tutor version on Palm
    Multi-tenant Elasticsearch prerequisites are not available till palm version

--- tutor-contrib-harmony-plugin/setup.py
+++ tutor-contrib-harmony-plugin/setup.py
@@ -40,7 +40,7 @@ setup(
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
     python_requires=">=3.7",
-    install_requires=["tutor"],
+    install_requires=["tutor>=16.0.0,<17.0.0"],
     entry_points={
         "tutor.plugin.v1": [
             "k8s_harmony = tutor_k8s_harmony_plugin.plugin",
