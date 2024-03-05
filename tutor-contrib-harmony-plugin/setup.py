9b074a022967f081c78b502976d7548b4f379ce6
jfavellar90 <jhony.avella@edunext.co>
2024-03-05 11:51:49 -0500

    chore: providing basic quince support

--- tutor-contrib-harmony-plugin/setup.py
+++ tutor-contrib-harmony-plugin/setup.py
@@ -39,8 +39,8 @@ setup(
     long_description=load_readme(),
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
-    python_requires=">=3.7",
-    install_requires=["tutor>=16.0.0,<17.0.0"],
+    python_requires=">=3.8",
+    install_requires=["tutor>=17.0.0,<18.0.0"],
     entry_points={
         "tutor.plugin.v1": [
             "k8s_harmony = tutor_k8s_harmony_plugin.plugin",
@@ -52,7 +52,6 @@ setup(
         "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
