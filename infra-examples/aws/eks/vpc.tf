edfbd51ab23a4a68e089ff1fb6fa08a3b930a315
henrrypg <henrry.pulgarin@edunext.co>
2024-10-17 11:32:29 -0500

    feat: add complete example of infrastructure with aws

---
+++ infra-examples/aws/eks/vpc.tf
@@ -0,0 +1,15 @@
+module "vpc" {
+  source          = "terraform-aws-modules/vpc/aws"
+  version         = "~> 5.13"
+  name            = var.vpc_name
+  cidr            = var.cidr
+  azs             = var.azs
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway     = var.enable_nat_gateway
+  single_nat_gateway     = var.single_nat_gateway
+  one_nat_gateway_per_az = var.one_nat_gateway_per_az
+
+  tags = var.tags
+}
