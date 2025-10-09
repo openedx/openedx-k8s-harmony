85dc605ee16e1a56884936c8b32641f84dda6026
Gabor Boros <gabor@opencraft.com>
2025-10-09 14:12:19 +0400

    refactor: make the s3 bucket name available
    Signed-off-by: Gabor Boros <gabor@opencraft.com>

--- terraform/modules/aws/s3/outputs.tf
+++ terraform/modules/aws/s3/outputs.tf
@@ -8,7 +8,13 @@ output "bucket_arn" {
   description = "The unique resource ID of the bucket."
 }
 
+output "bucket_name" {
+   value       = aws_s3_bucket.s3_bucket.name 
+   description = "The name of the S3 bucket."
+}
+
 output "bucket_domain_name" {
   value       = aws_s3_bucket.s3_bucket.bucket_domain_name
   description = "The domain name of the bucket, including the generated prefix."
 }
+
